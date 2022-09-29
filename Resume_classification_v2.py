de96097bd7cc7ecfa846dcd79a2ef0245165350a
ayushg9450 <66550569+ayushg9450@users.noreply.github.com>
2022-09-29 13:20:34 +0530

    Add files via upload

---
+++ Resume_classification_v2.py
@@ -0,0 +1,208 @@
+import pandas as pd
+import aspose.words as aw
+import streamlit as st
+from PIL import Image
+import PyPDF2
+import cv2
+import pickle
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+import pyperclip
+import os
+import string
+from streamlit_tags import st_tags
+from pyresparser import ResumeParser
+
+import textract
+#import nltk
+#nltk.download('stopwords')
+
+
+
+def pdf(pdf_file):
+    pdffileobj=open(pdf_file,'rb')
+    #data_1 = ResumeParser(pdfFile).get_extracted_data()
+    pdfReader = PyPDF2.PdfFileReader(pdffileobj)
+    num_pages = pdfReader.numPages
+    count = 0
+    text = ""
+    while count < num_pages:
+        pageObj = pdfReader.getPage(count)
+        count +=1
+        text += pageObj.extractText()
+        
+    return text
+    
+# def image(image_file):
+#     l = []
+#     img = cv2.imread(image_file)
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+#     rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+#     dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+#     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+#     cv2.CHAIN_APPROX_NONE)
+#     im2 = img.copy()
+#     for cnt in contours:
+#         x, y, w, h = cv2.boundingRect(cnt)
+#         rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+#         cropped = im2[y:y + h, x:x + w]
+#         text = pytesseract.image_to_string(cropped)
+#         l.append(text)
+#         wor = ['%','\n']
+#         for word in wor:
+#             for i in range(len(l)):
+#                 if word in l[i]:
+#                     l[i] = l[i].replace(word,'')
+#     return l
+
+
+import img2pdf
+from PIL import Image
+import os
+      
+def remove_brackets(data):
+    data = data.replace('[','')
+    data = data.replace(']','')
+    data = data.replace(',',' ')
+    data = data.replace('\'','')
+    #print(data)
+    return data
+
+
+
+def prediction(skills_data):
+    skills_data = str(skills_data)
+    skills = remove_brackets(skills_data)
+    with open('clf_model_pkl' , 'rb') as f:
+        clf = pickle.load(f)
+    with open('vectorizer_pkl' , 'rb') as f:
+        vec = pickle.load(f)
+    with open('le_pkl' , 'rb') as f:
+        le = pickle.load(f)
+    feature = vec.transform([skills])
+    predicted = clf.predict(feature)
+    st.success("** This Resume is suitable for {} Jobs **".format(le.inverse_transform(predicted)[0]))
+
+
+def save_uploadedfile(uploadedfile):
+    
+    with open(os.path.join(r"C:\Users\AyushKumar.Gupta\Downloads",uploadedfile.name),"wb") as f:
+        x = os.path.join(r"C:\Users\AyushKumar.Gupta\Downloads",uploadedfile.name)
+        f.write(uploadedfile.getbuffer())
+    return x
+
+def data_1(x):
+    resume_data = ResumeParser(x).get_extracted_data()  
+    resume_data_1 = resume_data['skills']
+    for i in range(len(resume_data_1)):
+        resume_data_1[i] = resume_data_1[i].lower()
+        
+    return resume_data_1
+    
+
+def data(x,key_):
+    
+    resume_data = ResumeParser(x).get_extracted_data()  
+    resume_data_1 = resume_data['skills']
+    for i in range(len(resume_data_1)):
+        resume_data_1[i] = resume_data_1[i].lower()
+    if resume_data:
+    ## Get the whole resume data
+
+        st.header("**Resume Analysis**")
+        st.success("Hello "+ resume_data['name'])
+        st.subheader("**Your Basic info**")
+        try:
+            st.text('Name: '+resume_data['name'])
+            st.text('Email: ' + resume_data['email'])
+            st.text('Contact: ' + resume_data['mobile_number'])
+            st.text('Resume pages: '+str(resume_data['no_of_pages']))
+        except:
+            pass
+
+    keywords = st_tags(label='### Skills that you have',
+            value=resume_data['skills'],key = key_)
+
+    #job_data = job_data['skills']
+
+    #resume_1 = resume_data['skills']
+    #count = 0
+    #for i in resume_1:
+     #   if i in job_data:
+      #      count +=1
+
+    #sim_score = count/len(job_data)
+    #sim_score = sim_score * 100
+    #st.success("Job Fittment: "+ str(sim_score))   
+
+
+    reco_field = prediction(resume_data_1)
+    return resume_data_1
+    
+    
+
+def main():
+    c=0
+    st.title("Resume Classifier")
+    menu = ["Resume_classification","Job_Match","About"]
+    choice = st.sidebar.selectbox("Menu",menu)
+    
+    if choice=="Resume_classification":
+        st.subheader("Files")
+        uploaded_files = st.file_uploader("Upload your resume", type=["pdf","docx","doc"],accept_multiple_files=True)
+        for uploaded_file in uploaded_files:
+            x = save_uploadedfile(uploaded_file)
+            if x.split('.')[-1] == 'doc':
+                doc = aw.Document(x)
+                x = os.path.join(r"C:\Users\AyushKumar.Gupta\Downloads",uploaded_file.name+'.pdf')
+                doc.save(x)
+                data(x)
+                
+            else:
+                data(x)
+                
+    elif choice == 'Job_Match':
+        st.subheader("Files")
+        uploaded_files = st.file_uploader("Upload candidate resume", type=["pdf","docx","doc"],accept_multiple_files=True)
+        job_files = st.file_uploader("Choose job description file", type=["pdf"],accept_multiple_files=True)
+        for job_file in job_files:
+            x = save_uploadedfile(job_file)
+            k = data_1(x)
+            for uploaded_file in uploaded_files:
+                c=c+1
+                x = save_uploadedfile(uploaded_file)
+                if x.split('.')[-1] == 'doc':
+                    doc = aw.Document(x)
+                    x = os.path.join(r"C:\Users\AyushKumar.Gupta\Downloads",uploaded_file.name+'.pdf')
+                    doc.save(x)
+                    s = data(x,str(c))
+                    count = 0
+                    for i in k:
+                        if i in s:
+                            count +=1
+
+                    sim_score = count/len(k)
+                    sim_score = sim_score * 100
+                    st.success("Job Fittment: "+ str(sim_score)) 
+
+                else: 
+                    s = data(x,str(c))
+                    count = 0
+                    for i in k:
+                        if i in s:
+                            count +=1
+
+                    sim_score = count/len(k)
+                    sim_score = sim_score * 100
+                    st.success("Job Fittment: "+ str(sim_score))          
+            
+
+    else:
+        st.subheader("About")
+        st.text("Built with streamlit and Python")
+
+if __name__=='__main__':
+    main()
---
+++ app.py
@@ -0,0 +1,123 @@
+import os
+import pickle
+import aspose.words as aw
+from flask_cors import CORS
+from pyresparser import ResumeParser
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+CORS(app)
+
+        
+def remove_brackets(data):
+    data = data.replace('[','')
+    data = data.replace(']','')
+    data = data.replace(',',' ')
+    data = data.replace('\'','')
+    #print(data)
+    return data
+
+def prediction(skills_data):
+    skills_data = str(skills_data)
+    skills = remove_brackets(skills_data)
+    with open('clf_model_pkl' , 'rb') as f:
+        clf = pickle.load(f)
+    with open('vectorizer_pkl' , 'rb') as f:
+        vec = pickle.load(f)
+    with open('le_pkl' , 'rb') as f:
+        le = pickle.load(f)
+    feature = vec.transform([skills])
+    predicted = clf.predict(feature)
+    return (le.inverse_transform(predicted)[0])
+
+def data_extraction_jd(x):
+    jd_data = ResumeParser(x).get_extracted_data()  
+    jd_data_skills = jd_data['skills']
+    for i in range(len(jd_data_skills)):
+        jd_data_skills[i] = jd_data_skills[i].lower()    
+    return jd_data_skills
+    
+
+def data_extraction_resume(x):
+    val = {} 
+    resume_data = ResumeParser(x).get_extracted_data() 
+    resume_data_skills = resume_data['skills']
+    for i in range(len(resume_data_skills)):
+        resume_data_skills[i] = resume_data_skills[i].lower()
+    if resume_data:
+        desgination = prediction(resume_data_skills)
+        try:
+            val = {
+            'name':resume_data['name'],
+            'email':resume_data['email'],
+            'contact': resume_data['mobile_number'],
+            'resume pages':str(resume_data['no_of_pages']),
+            'skills':resume_data['skills'],
+            'desgination predicted':desgination
+            }
+        except:
+            pass
+    return val
+       
+@app.route("/predict", methods = ['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save(secure_filename(file.filename))
+        temp = str(file.filename)
+        file_name = os.path.join(temp.replace(" ","_"))
+        if file_name.split('.')[-1] == 'doc':
+            doc = aw.Document(file_name)
+            file_name = os.path.join(r"C:\Users",file.filename+'.pdf')
+            doc.save(file_name)
+            out_ = data_extraction_resume(file_name)
+        else:
+            out_ = data_extraction_resume(file_name)  
+        return (jsonify(out_))
+    
+
+@app.route("/match", methods = ['POST'])
+def match():
+    if request.method == 'POST':
+        resumefile = request.files['file_resume']
+        descriptionfile = request.files['file_description']
+        
+        resumefile.save(secure_filename(resumefile.filename))
+        temp = str(resumefile.filename)
+        resume_file = os.path.join(temp.replace(" ","_"))
+        
+        descriptionfile.save(secure_filename(descriptionfile.filename))
+        temp = str(descriptionfile.filename)
+        description_file = os.path.join(temp.replace(" ","_"))
+        jd_skill_set = data_extraction_jd(description_file)
+        
+        if resume_file.split('.')[-1] == 'doc':
+            doc = aw.Document(resume_file)
+            resume_file = os.path.join(r"C:\Users",resumefile.filename+'.pdf')
+            doc.save(resume_file)
+            basic_data = data_extraction_resume(resume_file)
+            candidate_skills_set = data_extraction_resume(resume_file)['skills']
+            count = 0
+            for i in jd_skill_set:
+                if i in candidate_skills_set:
+                    count +=1
+
+            sim_score = count/len(jd_skill_set)
+            sim_score = sim_score * 100
+            basic_data['job fittment'] = str(sim_score)
+            return (jsonify(basic_data))
+        else:
+            basic_data = data_extraction_resume(resume_file)
+            candidate_skills_set = data_extraction_resume(resume_file)['skills']
+            count = 0
+            for i in jd_skill_set:
+                if i in candidate_skills_set:
+                    count +=1
+
+            sim_score = count/len(jd_skill_set)
+            sim_score = sim_score * 100
+            basic_data['job fittment'] = str(sim_score)
+            return (jsonify(basic_data))      
+ 
+if __name__ == '__main__':
+    app.run(debug = True)    
